bfcaf19de2388ef78fe4363d65c7266fb5417b98
sebPV <sebastianpvange@gmail.com>
2025-10-20 19:02:15 +0200

    setup

---
+++ aPythonPack/__init__.py
@@ -0,0 +1,25 @@
+
+
+
+import matplotlib.pyplot as plt  # importerer matplotlib
+import sympy as sp# importerer sympy
+from sympy import exp, I , pi, sqrt
+import numpy as np
+sp.init_printing()  # gør at matematiske udtryk skrives pænt
+
+def simpArr(arr):
+    for n in range(len(arr)):
+        arr[n] = sp.simplify(arr[n])
+    return arr
+def sympArr(arr):
+    for n in range(len(arr)):
+        arr[n] = sp.sympify(arr[n])
+    return arr
+def forceRekt(arr):
+    for n in range(len(arr)):
+        arr[n] = sp.re(arr[n])+i*sp.im(arr[n])
+    return arr
+def forcePol(arr):
+    for n in range(len(arr)):
+        arr[n] = sp.Abs(arr[n])*sp.exp(i*sp.arg(arr[n]))
+    return arr
