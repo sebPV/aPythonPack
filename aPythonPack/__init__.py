e9436122882482e3375c2fa1f3dd8c0300dab985
sebPV <sebastianpvange@gmail.com>
2025-10-20 20:09:44 +0200

    add plot

--- aPythonPack/__init__.py
+++ aPythonPack/__init__.py
@@ -2,21 +2,50 @@ import matplotlib.pyplot as plt  # importerer matplotlib
 import sympy as sp# importerer sympy
 from sympy import exp, I , pi, sqrt
 import numpy as np
+from collections.abc import Iterable
 sp.init_printing()  # gør at matematiske udtryk skrives pænt
 
 def simpArr(arr):
-    for n in range(len(arr)):
-        arr[n] = sp.simplify(arr[n])
-    return arr
+    res = []
+    for n in arr:
+        res.append(sp.simplify(n))
+    return res
 def sympArr(arr):
-    for n in range(len(arr)):
-        arr[n] = sp.sympify(arr[n])
-    return arr
+    res = []
+    for n in arr:
+        res.append(sp.sympify(n))
+    return res
+
 def forceRekt(arr):
-    for n in range(len(arr)):
-        arr[n] = sp.re(arr[n])+I*sp.im(arr[n])
-    return arr
+    if isinstance(arr, Iterable):
+        res = []
+        for n in range(len(arr)):
+            res.append(sp.re(n)+I*sp.im(n))
+        return res
+    else:
+        return sp.re(arr)+I*sp.im(arr)
 def forcePol(arr):
-    for n in range(len(arr)):
-        arr[n] = sp.Abs(arr[n])*sp.exp(I*sp.arg(arr[n]))
-    return arr
+    if isinstance(arr,Iterable):
+        res = []
+        for n in arr:
+            res.append(sp.Abs(n)*sp.exp(I*sp.arg(n)))
+        return res
+    else:
+        return sp.Abs(arr)*sp.exp(I*sp.arg(arr))
+
+def plotComplex(arr):
+    plt.axvline(0, color="gray")  # akser
+    plt.axhline(0, color="gray")  # akser
+    Rea = []
+    Ima = []
+    for z in arr:
+        Rea.append(sp.re(z))
+        Ima.append(sp.im(z))
+        plt.annotate(r'$s$'.replace('s',sp.latex(z)),(sp.re(z),sp.im(z)))
+        
+    plt.scatter(Rea, Ima)
+    plt.xlabel(r"$Re(z)$")  # akse-etiketter
+    plt.ylabel(r"$Im(z)$")  # akse-etiketter
+    plt.grid(True)  # gitter
+
+print(forcePol(2+3*I))
