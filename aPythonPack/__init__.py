9bf0134c9f97530a4965718e21734752e0b98801
sebPV <sebastianpvange@gmail.com>
2025-10-21 14:54:48 +0200

    fix file

--- aPythonPack/__init__.py
+++ aPythonPack/__init__.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt  # importerer matplotlib
 import sympy as sp# importerer sympy
 from sympy import exp, I , pi, sqrt, E, log, ln
+from sympy import E as e
 import numpy as np
 from collections.abc import Iterable
 sp.init_printing()  # gør at matematiske udtryk skrives pænt
-import LatexConversions
+# import LatexConversions
 def simpArr(arr):
     res = []
     for n in arr:
@@ -80,3 +81,36 @@ def plotComplex(arr):
     plt.ylabel(r"$Im(z)$")  # akse-etiketter
     plt.grid(True)  # gitter
 
+
+def arrTabular(*Data,hline=False):
+    res=''
+    subArr = []
+    subArrLen = []
+    for n in Data:
+        if isinstance(n[0], Iterable):
+            for m in n:
+                subArr.append(m)
+                subArrLen.append(len(m))
+        else:
+            subArr.append(n)
+            subArrLen.append(len(n))
+    
+    for n in range(max(subArrLen)):
+        line = ''
+        for m in subArr:
+            if not n < len(m):
+                line = line + '  & '
+            elif isinstance(m[n],sp.Expr):
+                line = line +'$ r $ & '.replace('r',sp.latex(m[n]))
+            elif isinstance(m[n],(int, float)):
+                line = line + '$ r $ & '.replace('r',str(m[n]))
+            else:
+                line = line + str(m[n]) + ' & '
+        line = line[:-2] + ' \\\\ \n'
+        if hline:
+            line = line + ' \\hline \n'
+        res = res + line
+    return res
+
+print(arrTabular([3,4,5,6],sp.Matrix([[1,2],[3,4]]),hline = True))
+print(sp.Matrix([[1,2],[3,4]]))
