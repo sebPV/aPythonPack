976b9d90c1ceb743e9ae55ebed61d52322a0ed3c
sebPV <44200085+sebPV@users.noreply.github.com>
2026-02-02 12:20:55 +0100

    chage to dftotab

--- testScript.py
+++ testScript.py
@@ -6,27 +6,22 @@ df = pd.DataFrame(
         'ShaftSpeedIndex':pd.Series([1200,1200,1200,1600,1600,1600,2300,2300,2300],dtype='pint[rpm]'),
         'FlowRate':pd.Series([8.72,9.28,9.31,11.61,12.78,13.51,18.32,17.90,19.23],dtype='pint[m^3/h]'),
         'FlowRateNU':pd.Series([8.72,9.28,9.31,11.61,12.78,13.51,18.32,17.90,19.23])
-    }
+    },
+    # index = [1200,1200,1200,1600,1600,1600,2300,2300,2300]
 )
 
 df.ShaftSpeedIndex = df.ShaftSpeedIndex.pint.to('Hz')
-
+print(df)
 
 def dfToTab(Data=pd.DataFrame):
-    tabular = ''
-    header = ''
+    outData = pd.DataFrame()
     for i in Data:
-        header += ' ' + str(i)
         if str(type(df[i].dtypes)) == '<class \'pint_pandas.pint_array.PintType\'>':
-            header += ' [ {:~L}'.format(df[i].pint.units) +']'
-        header += ' &'
-    tabular += header[:-1] + '\\\ \n \\hline \n'
+            outData[i+ ' [ {:~L}'.format(df[i].pint.units) +']'] = df[i].values.quantity.magnitude
+        else:
+            outData[i] = df[i]
+    return outData.to_latex()
 
-    return tabular
-
-# for i in df:
-#     print(str(type(df[i].dtypes)))
-
-
+print(dfToTab(df))
 
 # print()
